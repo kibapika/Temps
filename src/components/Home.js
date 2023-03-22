9e9e4d2317fc8b65322bcbfa4ac6acc297cb0a69
Eva Li <evali3101@gmail.com>
2023-03-22 16:38:09 -0400

    movile friendly

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,14 +3,14 @@ import ThreeDays from "./ThreeDays";
 import Current from "./Current";
 
 const Home = () => {
-    return (
-        <div className="background py-10">
-            <h1 className="text-3xl">Welcome to Temps</h1>
-            <p>"Interactive Weather App"</p>
-            <Current />
-            <ThreeDays/>
-        </div>
-    )
-}
+  return (
+    <div className="background">
+      <h1 className="text-3xl">Welcome to Temps</h1>
+      <p>"Interactive Weather App"</p>
+      <Current />
+      <ThreeDays />
+    </div>
+  );
+};
 
 export default Home;
